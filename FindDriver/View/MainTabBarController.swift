2f96c51f5e24bb2d5e4edf83bb4fc5eda256a064
Miad <Mead_az@yahoo.com>
2019-03-30 19:10:38 -0400

    "Before Add Table Views to tab bars"

---
+++ FindDriver/View/MainTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabController.swift
+//  FindDriver
+//
+//  Created by Miad Azarmehr on 2019-03-30.
+//  Copyright © 2019 Miad Azarmehr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+    
+}
